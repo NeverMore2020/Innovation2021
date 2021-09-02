c131bcabce84face29f33343103479a55cea721a
eccente <2018212612@bupt.edu.cn>
2021-09-02 16:37:55 +0800

    搭建首页

---
+++ miniprogram-1/pages/main/main.js
@@ -0,0 +1,79 @@
+// pages/main.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    types:[
+      { id:0, icon:'../../static/icon1-通用-主食.svg', name:'主食类' },
+      { id:1, icon:'../../static/icon1-通用-鸡腿.svg', name:'肉蛋类' },
+      { id:2, icon:'../../static/蚕豆.svg', name:'大豆及制品' },
+      { id:3, icon:'../../static/生鲜-蔬菜.svg', name:'蔬菜菌藻类' },
+      { id:4, icon:'../../static/icon1-通用-水果.svg', name:'水果类' },
+      { id:5, icon:'../../static/牛奶.svg', name:'奶类' },
+      { id:6, icon:'../../static/油壶.svg', name:'油脂类' },
+      { id:7, icon:'../../static/坚果.svg', name:'坚果类' },
+      { id:8, icon:'../../static/调味品.svg', name:'调味品' },
+      { id:9, icon:'../../static/icon1-通用-饮料.svg', name:'饮料类' },
+      { id:10, icon:'../../static/icon1-通用-甜甜圈.svg', name:'零食及冷饮' },
+      { id:11, icon:'../../static/其他.svg', name:'其他' },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
